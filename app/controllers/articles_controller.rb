e736897855d7ad6b2590c2041774019d855e0611
Ofir Dahan <ofirdahan112@gmail.com>
2018-02-20 21:20:30 -0800

    added edit button to show page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,14 @@
+# Articles Controller
 class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
 
   def show
+    @article = Article.find(params[:id])
+  end
+
+  def edit
     @article = Article.find(params[:id])
   end
 
@@ -14,6 +19,16 @@ class ArticlesController < ApplicationController
       redirect_to article_path(@article)
     else
       render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = 'article updated!'
+      redirect_to article_path(@article)
+    else
+      render 'edit'
     end
   end
 
