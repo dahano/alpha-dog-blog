99165d822cb2117b0a0616f8bc85d6e31386c1dc
Ofir Dahan <ofirdahan112@gmail.com>
2018-04-16 22:05:09 -0700

    added more integration tests

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,8 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
-  
+
   resources :users, except: [:new]
   resources :articles
+  resources :categories, except: [:destroy]
 end
